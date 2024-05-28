016f5423f84dbe24d84aff7793c002b1b45f2428
yemiancheng <ymc.github@gmail.com>
2024-05-28 19:58:31 +0800

    core(core): let banner text to slim as possibility

--- vite-config-util/nano/src/main.ts
+++ vite-config-util/nano/src/main.ts
@@ -2,18 +2,61 @@
 // recommended-lib-name: banz-strm-from-flag,@nano/banz-strm-from-flag,@banz/strm-from-flag
 // keywords: nano,banz,strm-from-flag
 
-export interface BanzFlag {
-  // [x: string]: string | undefined;
-  name: string;
-  version: string;
-  description: string;
-  author: string;
-  homepage: string;
+// export interface BanzFlag {
+//   // [x: string]: string | undefined;
+//   name: string;
+//   version: string;
+//   description: string;
+//   author: string;
+//   homepage: string;
+// }
+// export type BanzFlagLike = Partial<BanzFlag>;
+// export function makeBannerText(pkg: BanzFlagLike) {
+//   return `/**\n * name: ${pkg.name}\n * version: v${pkg.version}\n * description: ${pkg.description}\n * author: ${pkg.author}\n * homepage: ${pkg.homepage}\n */`;
+// }
+
+export type PkgInfo = Record<string, any>;
+function cookBannerKeys(keys: string | string[]) {
+  return typeof keys == "string"
+    ? keys.split(",").filter((v) => v.trim())
+    : keys;
 }
-export type BanzFlagLike = Partial<BanzFlag>;
-export function makeBannerText(pkg: BanzFlagLike) {
-  return `/**\n * name: ${pkg.name}\n * version: v${pkg.version}\n * description: ${pkg.description}\n * author: ${pkg.author}\n * homepage: ${pkg.homepage}\n */`;
+// function cookBannerData(data: PkgInfo, keys: string | string[]) {
+//   let ls = cookBannerKeys(keys);
+//   let res: Record<string, any> = {};
+//   for (let index = 0; index < ls.length; index++) {
+//     const key = ls[index];
+//     res[key] = data[key];
+//     // if (!res[key]) res[key] = "";
+//   }
+//   return res;
+// }
+/**
+ *
+ * @sample
+ * ```
+ * import pkg from "./package.json";
+ * let banner = cookBannerText(pkg, "name,version");
+ * ```
+ */
+export function cookBannerText(
+  data: PkgInfo,
+  keys: string | string[] = "name,version,description,author,homepage"
+) {
+  let ls = cookBannerKeys(keys);
+  let res: string[] = ["/**"];
+  for (let index = 0; index < ls.length; index++) {
+    const key = ls[index];
+    let val = data[key];
+    if (val) {
+      res.push(` * ${key}:${val}`);
+    }
+  }
+  res.push(` */`);
+  return res.join("\n");
 }
+
+export { cookBannerText as makeBannerText };
 
 // a library package to make umd exports.name
 // recommended-lib-name: umd-name-std,@nano/umd-name-std,@umd/name-std
